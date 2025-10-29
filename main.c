035d65e19dcce6b114a541fd7733a0a0232c982e
BlueGeckoJP <kei09doya@gmail.com>
2025-10-29 23:02:28 +0900

    style: add .clang-format (Mozilla style) and reformat main.c; update .gitignore to ignore /.vscode

--- main.c
+++ main.c
@@ -1,27 +1,31 @@
-// the following definition is required to enable POSIX.1b features such as nanosleep
+// the following definition is required to enable POSIX.1b features such as
+// nanosleep
 #define _POSIX_C_SOURCE 199309L
 
 #include <notcurses/notcurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    notcurses_options opts = {};
-    struct notcurses *nc = notcurses_core_init(&opts, NULL);
-    if (nc == NULL) {
-        fprintf(stderr, "Error initializing Notcurses\n"); // NOLINT
-        return EXIT_FAILURE;
-    }
+int
+main(void)
+{
+  notcurses_options opts = {};
+  struct notcurses* nc = notcurses_core_init(&opts, NULL);
+  if (nc == NULL)
+  {
+    fprintf(stderr, "Error initializing Notcurses\n"); // NOLINT
+    return EXIT_FAILURE;
+  }
 
-    struct ncplane *std = notcurses_stdplane(nc);
+  struct ncplane* std = notcurses_stdplane(nc);
 
-    ncplane_putstr(std, "Hello, world!\n");
-    notcurses_render(nc);
+  ncplane_putstr(std, "Hello, world!\n");
+  notcurses_render(nc);
 
-    struct timespec ts = {.tv_sec = 2, .tv_nsec = 0};
-    nanosleep(&ts, NULL);
+  struct timespec ts = { .tv_sec = 2, .tv_nsec = 0 };
+  nanosleep(&ts, NULL);
 
-    notcurses_stop(nc);
+  notcurses_stop(nc);
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
